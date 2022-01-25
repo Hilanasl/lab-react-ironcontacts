77d028996c86e2cc8d3d15334907f066aff9b12c
Hilana <hilana.sene@gmail.com>
2022-01-25 20:24:39 +0100

    done

--- src/App.js
+++ src/App.js
@@ -1,37 +1,93 @@
 import logo from './logo.svg';
 import './App.css';
-import contacts from './contacts.json'
+import contactJson from './contacts.json'
+import {useState} from 'react';
+
+const fiveContacts = contactJson.slice(0,5)
 
 function App() {
+  const trophy = '🏆' 
+
+  const [contacts, setContacts] = useState(fiveContacts)
+
+  const addRandom = () => {
+    // If we want to check that the randomContact is not already in our contacts Array
+    // get our random contact id
+    // Try to find in our state contacts if we have a corresponding Id
+    // If so ? What shall we do
+
+    // 1 Get a random contact
+  const randomContact = contactJson[Math.floor(Math.random() * contactJson.length)];
+  // 2 Make a copy of you state (contact) you can use a spread :) 
+    const copy = [...contacts]
+  // 3 Add (push) the random contatc to the copy !
+    copy.push(randomContact)
+  // 4 set the copy as the new state  (setContacts)
+  setContacts(copy)
+}
+
+const sortByName = () => {
+  const copy = [...contacts];
+  copy.sort((a, b) => {
+    return a.name.localeCompare(b.name)
+  })
+  setContacts(copy);
+}
+
+const sortByPopularity = () => {
+  const copy = [...contacts];
+  copy.sort((a, b) => {
+    return b.popularity - a.popularity
+  })
+  setContacts(copy);
+}
+
+const deleteContact = (id) => {
+  console.log(id)
+const filteredContacts = contacts.filter(item => {
+  return item.id !== id
+})
+  setContacts(filteredContacts)
+}
+
+
   return (
-    <table>
-      [
-  {
-    name: "Idris Elba",
-    pictureUrl: "https://image.tmdb.org/t/p/w500/d9NkfCwczP0TjgrjpF94jF67SK8.jpg",
-    popularity: {11.622713},
-  },
-  {
-    name: "Johnny Depp",
-    pictureUrl: "https://image.tmdb.org/t/p/w500/kbWValANhZI8rbWZXximXuMN4UN.jpg",
-    popularity: 15.656534,
-  },
-  {
-    "name": "Monica Bellucci",
-    "pictureUrl": "https://image.tmdb.org/t/p/w500/qlT4904d8oi2NIs28RrgnIZDFZB.jpg",
-    "popularity": 16.096436,
-  },
-  {
-    "name": "Gal Gadot",
-    "pictureUrl": "https://image.tmdb.org/t/p/w500/34kHAyBaBhq2kUrmhM15paEBuuI.jpg",
-    "popularity": 10.049256,
-  },
-  {
-    "name": "Ian McKellen",
-    "pictureUrl": "https://image.tmdb.org/t/p/w500/coWjgMEYJjk2OrNddlXCBm8EIr3.jpg",
-    "popularity": 10.070132,
-  }]
-    </table>
+    <div className='App'>
+      <h1>IronContacts</h1>
+      <div className='btns'>
+      <button onClick={() => addRandom()}>Add Random Contact</button>
+      <button onClick={sortByPopularity}>Sort by popularity</button>
+      <button onClick={sortByName}>Sort by name</button>
+      </div>
+      <div className='contactsTable'>
+        <table>
+          <thead>
+            <th><b>Picture</b></th>
+            <th><b>Name</b></th>
+            <th><b>Popularity</b></th>
+            <th><b>Won an Oscar</b></th>
+            <th><b>Won an Emmy</b></th>
+            <th><b>Actions</b></th>
+          </thead>
+          <tbody>
+            {contacts.map(contact => {
+              return(
+                <tr key={contact.id}>
+                  <td><img src={contact.pictureUrl} width={50} alt=""/></td>
+                  <td>{contact.name}</td>
+                  <td>{contact.popularity}</td>
+                  <td>{contact.wonOscar && trophy}</td>
+                  <td>{contact.wonEmmy && trophy}</td>
+                  <td><button className="deleteBtn" onClick={() => deleteContact(contact.id)}>Delete</button></td>
+                </tr>
+              )
+              })
+            }
+          </tbody>
+        </table>
+      </div>
+    </div>
+    
   );
 }
 
